8c8b9ab4e594eb9c02b7b2ccfe95f6fe4ce3f972
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-05-18 11:49:42 +0700

    initial commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,165 @@
+const ROUTES = [
+  {
+    path: "home",
+    title: "KepoBro News - Kepoin tentang yang hits saat ini!",
+  },
+  {
+    path: "about",
+    title: "KepoBro News - About Developer",
+  },
+  {
+    path: "contact",
+    title: "KepoBro News - Contact Developer",
+  },
+  {
+    path: "404",
+    title: "KepoBro News - Page Not Found",
+  },
+];
+
+const methods = {
+  getSelectedRoute: (path) => {
+    return ROUTES.find((route) => {
+      return route.path === path || route.path === "404";
+    });
+  },
+
+  initializeSidebarMenus: () => {
+    document.querySelectorAll(".sidenav a").forEach((element) => {
+      element.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const page = event.target.getAttribute("href");
+        const sidenav = document.querySelector(".sidenav");
+        const selectedRoute = methods.getSelectedRoute(page);
+
+        M.Sidenav.getInstance(sidenav).close();
+        methods.navigate(selectedRoute);
+      });
+    });
+  },
+
+  initializeButtonLogo: () => {
+    const button = document.querySelector("#BtnLogo");
+    const page = button.getAttribute("href");
+    const selectedRoute = methods.getSelectedRoute(page);
+
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      methods.navigate(selectedRoute);
+    });
+  },
+
+  loadPage: (page) => {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+      if (this.readyState !== 4) return;
+
+      const content = document.querySelector(".Main .Main-body");
+
+      switch (this.status) {
+        case 200: {
+          content.innerHTML = xhttp.responseText;
+          break;
+        }
+
+        case 404: {
+          const selectedRoute = methods.getSelectedRoute("404");
+          methods.navigate(selectedRoute);
+          break;
+        }
+
+        default: {
+          content.innerHTML = this.status;
+        }
+      }
+    };
+
+    xhttp.open("GET", `pages/${page}.html`, true);
+    xhttp.send();
+  },
+
+  navigate: (state) => {
+    const { path, title } = state;
+    const url = `/#/${path}`;
+
+    history.pushState(state, null, url);
+
+    methods.setDocumentTitle(title);
+    methods.loadPage(path);
+  },
+
+  registerServiceWorker: () => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker
+          .register("service-worker.js")
+          .then(() => console.log("Service worker registration success."))
+          .catch((error) => {
+            console.error("Error during service worker registration:", error);
+          });
+      });
+    }
+  },
+
+  setDocumentTitle: function (title) {
+    document.title = title;
+  },
+
+  showOfflineToast: function () {
+    M.Toast.dismissAll();
+    M.toast({
+      html: "You are currently offline.",
+      classes: "red",
+      displayLength: 36000,
+      outDuration: 0,
+    });
+  },
+
+  showOnlineToast: function () {
+    M.Toast.dismissAll();
+    M.toast({
+      html: "Connected",
+      classes: "green",
+      outDuration: 0,
+    });
+  },
+};
+
+const mounted = () => {
+  const path = window.location.hash.substr(2) || "home";
+  const { title } = methods.getSelectedRoute(path);
+  const sidenav = document.querySelectorAll(".sidenav");
+
+  M.Sidenav.init(sidenav);
+
+  methods.initializeSidebarMenus();
+  methods.initializeButtonLogo();
+  methods.setDocumentTitle(title);
+  methods.loadPage(path);
+};
+
+window.addEventListener("popstate", (event) => {
+  let { state } = event;
+
+  if (!state) {
+    state = methods.getSelectedRoute("home");
+  }
+
+  methods.setDocumentTitle(state.title);
+  methods.loadPage(state.path);
+});
+
+window.addEventListener("offline", () => {
+  methods.showOfflineToast();
+});
+
+window.addEventListener("online", () => {
+  methods.showOnlineToast();
+});
+
+document.addEventListener("DOMContentLoaded", mounted());
+
+methods.registerServiceWorker();
---
+++ service-worker.js
@@ -0,0 +1,96 @@
+const ICON_SIZES = ["72", "96", "128", "144", "152", "192", "384", "512"];
+const IMAGES = ["404.png", "logo.png", "photo.jpg"];
+const SCRIPTS = ["index", "materialize.min"];
+const STYLES = ["index", "materialize.min"];
+
+const getAssetFiles = (fileType) => {
+  const folderPath = "/assets";
+
+  switch (fileType) {
+    case "icon": {
+      const iconFiles = ICON_SIZES.map(
+        (iconSize) => `${folderPath}/icons/icon-${iconSize}x${iconSize}.png`
+      );
+
+      return iconFiles;
+    }
+
+    case "image": {
+      const imageFiles = IMAGES.map((image) => `${folderPath}/images/${image}`);
+
+      return imageFiles;
+    }
+
+    case "script": {
+      const scriptFiles = SCRIPTS.map(
+        (script) => `${folderPath}/scripts/${script}.js`
+      );
+
+      return scriptFiles;
+    }
+
+    case "style": {
+      const styleFiles = STYLES.map(
+        (style) => `${folderPath}/styles/${style}.css`
+      );
+
+      return styleFiles;
+    }
+  }
+};
+
+const CACHE = {
+  name: "kepobro-v1",
+  urls: [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/pages/about.html",
+    "/pages/home.html",
+    "/pages/contact.html",
+    ...getAssetFiles("icon"),
+    ...getAssetFiles("image"),
+    ...getAssetFiles("script"),
+    ...getAssetFiles("style"),
+  ],
+};
+
+self.addEventListener("install", (event) => {
+  const preCache = async () => {
+    const cache = await caches.open(CACHE.name);
+
+    return cache.addAll(CACHE.urls);
+  };
+
+  event.waitUntil(preCache());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE.name);
+      const cachedResponse = await cache.match(event.request);
+
+      if (cachedResponse) {
+        event.waitUntil(cache.add(event.request));
+        return cachedResponse;
+      }
+
+      return fetch(event.request);
+    })()
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      const updatedCache = cacheNames.map((cacheName) => {
+        if (cacheName !== CACHE.name) {
+          return caches.delete(cacheName);
+        }
+      });
+
+      return Promise.all(updatedCache);
+    })
+  );
+});
